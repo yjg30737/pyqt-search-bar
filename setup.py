cc5e177220be4c0c72e4f45c16a476cadc489712
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-23 09:52:52 +0900

    Add new imported packages(pyqt-resource-helper, pyqt-svg-icon-pushbutton)

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setup(
     license='MIT',
     packages=find_packages(),
     package_data={'pyqt_search_bar.style': ['button.css', 'lineedit.css'],
-                  'pyqt_search_bar.ico': ['close.png', 'search.png']},
+                  'pyqt_search_bar.ico': ['close.png', 'search.svg']},
     description='PyQt search bar',
     url='https://github.com/yjg30737/pyqt-search-bar.git',
     install_requires=[
-        'PyQt5>=5.8'
+        'PyQt5>=5.8',
+        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main',
+        'pyqt-svg-icon-pushbutton @ git+https://git@github.com/yjg30737/pyqt-svg-icon-pushbutton.git@main'
     ]
 )
